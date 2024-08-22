2f4ed76968344781a2714090605c073c86ff24ff
Ron Olson <tachoknight@gmail.com>
2024-08-22 08:20:04 -0500

    First version for Discord

--- tempbot.py
+++ tempbot.py
@@ -5,103 +5,28 @@ import collections
 import json
 import configparser
 import paho.mqtt.client as mqtt
-from slack import RTMClient
+
+import discord
+from discord.ext import commands
 
 ##########################################################################################
 ## Configuration
 ##########################################################################################
 
 config = configparser.ConfigParser()
-config.read('config.ini')
-
-
-##########################################################################################
-## Our thread-safe dictionary
-##########################################################################################
-
-class TempDict:
-    def __init__(self):
-        self.lock = threading.Lock()
-        self.data = {}
-
-    def __getitem__(self, key):
-        with self.lock:
-            return self.data[key]
-
-    def __setitem__(self, key, value):
-        with self.lock:
-            self.data[key] = value
-
-    def __len__(self):
-        with self.lock:
-            return len(self.data)
-
-# This is the actual dictionary we're going to populate
-temp_dict = TempDict()
-
+config.read("config.ini")
 
 ##########################################################################################
-## MQTT Section
+## Discord fun
 ##########################################################################################
 
-def handle_temp_message(message):
-    # The message we get here looks like:
-    #   "MQTT publish: topic 'ps1/zigbee2mqtt/tempsense-lasers', payload '{\"battery\":100,\"humidity\":24.09,\"linkquality\":65,\"temperature\":17.39,\"voltage\":3100}'"
-    # which we split on ', payload' so the resulting array has 
-    # the area in msg_parts[0], and the actual payload in msg_parts[1]
-    # that we will convert to json
-    msg_parts = message.split(', payload')
+intents = discord.Intents.all()
 
-    # The part that will give us the sensor name
-    # This crazy line is simply to get us directly to the sensor
-    # name, skipping all the intermediate parts
-    area = msg_parts[0].split("'")[1::2][0].split('/')[2].split('-')[1]
-    # And now the actual json payload
-    payload = json.loads(msg_parts[1].strip().replace("'", ""))
-
-    # And add it to our dictionary
-    temp_dict[area] = payload
-
-    print(f"AREA: {area} ==> {temp_dict[area]}")
-    
-# This function is invoked by the MQTT library when we get
-# a message
-def on_message(client, userdata, message):
-    try:
-        payload = str(message.payload.decode())
-        # convert string to a dictionary
-        payload_dict = json.loads(payload)    
-        # There are lots of different types of payloads, we
-        # want the one that explicitly has the "message" key
-        # because we need to know what sensor we're working with,
-        # and if that key exists, then we only want the messages
-        # where "tempsense" is in the text, as those are the messages
-        # we're working with
-        if 'message' in payload_dict and 'tempsense' in payload_dict['message']:        
-            handle_temp_message(payload_dict['message'])
-    except Exception as e:
-        print("Got a message error: {str(e)}")
-
-def setup_mqtt_reader():
-    # Initialize the MQTT client and connect to the broker
-    client = mqtt.Client()
-    client.connect(config['MQTT']['broker'], int(config['MQTT']['port']))
-
-    # Subscribe to the MQTT topic
-    client.subscribe(config['MQTT']['topic'])
-
-    # Add the message callback function to the MQTT client
-    client.on_message = on_message
-
-    # Start the MQTT client loop to receive messages
-    client.loop_forever()
+# Create an instance of a bot
+bot = commands.Bot(description="Temp Bot", command_prefix="!", intents=intents)
 
 
-##########################################################################################
-## Slack Section
-##########################################################################################
-
-# This is where we build the text that will be sent back to the requestor. 
+# This is where we build the text that will be sent back to the requestor.
 def build_response(request):
     # This sub-function will loop through the array and return the first element
     # value where the length of the string is greater than 0
@@ -109,25 +34,27 @@ def build_response(request):
         for part in parts_array:
             if len(part) > 0:
                 return part
-        return ''
+        return ""
 
     def build_help_text():
-        help_response = 'Hmm, you want to enter `!temp <area>` (case insensitive).\n_I currently know of the following areas:_ '
+        help_response = "Hmm, you want to enter `!temp <area>` (case insensitive).\n_I currently know of the following areas:_ "
         # Now get all the areas we know about
         areas = list(temp_dict.data.keys())
-        area_text = ''
-        for area in areas:            
-            area_text += f'`{area}`,' 
-        
+        area_text = ""
+        for area in areas:
+            area_text += f"`{area}`,"
+
         # And, uh, remove the last comma
         help_response += area_text[:-1]
-        help_response += '\nYou can also type `!temp all` to get the temperatures in all areas'
+        help_response += (
+            "\nYou can also type `!temp all` to get the temperatures in all areas"
+        )
         return help_response
 
     def build_temp_text(area_requested):
         # This is the function that we use to build the response that
         # will have the temperature and humidty, that we'll format in
-        # both fahrenheit and celsius  
+        # both fahrenheit and celsius
         def format_temp(stored_area, temp_json):
             # Your standard conversion...
             def celsius_to_fahrenheit(celsius):
@@ -136,33 +63,33 @@ def build_response(request):
                 return round(fahrenheit)
 
             # Get our values...
-            cel_temp = temp_json['temperature']
+            cel_temp = temp_json["temperature"]
             fah_temp = celsius_to_fahrenheit(cel_temp)
-            humidity = temp_json['humidity']
+            humidity = temp_json["humidity"]
 
             # And now let's build our string
             area_temp_text = f"The temperature in `{stored_area}` is *{fah_temp}* F (*{cel_temp}* C). The humidty is *{humidity}*."
-           
+
             return area_temp_text
 
         # Okay, we have an area, but is that a _real_ area or gibberish?
-        temp_text = ''
-        
+        temp_text = ""
+
         # Let's go through the keys to see if we can find what they asked for
         areas = list(temp_dict.data.keys())
         for area in areas:
             # Special ... if they want everything, we build that here
-            if area_requested == 'all':
+            if area_requested == "all":
                 temp_text += format_temp(area, temp_dict[area])
                 # And get ready for the next line
-                temp_text += "\n"                
+                temp_text += "\n"
             # Did we find the specific area?
             elif area == area_requested:
                 # Ah, we did, so we'll build the response by
                 # formatting the json that holds the temperature,
                 # humidity, etc.
                 temp_text = format_temp(area, temp_dict[area])
-        
+
         # Did we find the appropriate area?
         if len(temp_text) == 0:
             # No we didn't, so we'll give them the help text.
@@ -171,16 +98,16 @@ def build_response(request):
         return temp_text
 
     print(f"Got this request: {request}")
-    
+
     # This is what we're gonna send back
-    response = ''
+    response = ""
 
     # First let's see if the request contains a specific area or the magic word "all"
     # We split on the space and get an array of parts we're going to work with
-    req_parts = request.split(' ')
-    # req_parts[0] is the trigger word ('!temp'). 
+    req_parts = request.split(" ")
+    # req_parts[0] is the trigger word ('!temp').
     # We may or may not have another word after the trigger word, and
-    # we don't know how many spaces are between element 0 and the word. 
+    # we don't know how many spaces are between element 0 and the word.
     # Soooo we have a sub-function that looks to see if we can find
     # something. _If_ we do find a word, we'll try to find that in the
     # dictionary. Otherwise, if we can't find the word, or there's nothing,
@@ -199,27 +126,103 @@ def build_response(request):
 
     return response
 
-@RTMClient.run_on(event="message")
-def ask_for_temp(**payload):
-    data = payload['data']
-    web_client = payload['web_client']
 
-    # We are listening for the "!temp" command, which we'll then 
-    # use to see if they want everything, or a particular area,
-    # or nothing, in which case we'll show the help
-    if '!temp' in data['text'].lower():
-        # Send the text to the fuction and get the response back which
-        # is what we'll tell the person
-        response = build_response(data['text'].lower())
+# Define a command
+@bot.command(name="temp")
+async def hello_world(ctx):
+    # Get the message that was sent to us
+    request = ctx.message.content
+    # And build the response
+    response = build_response(request)
+    # And send it back
+    await ctx.send(response)
 
-        channel_id = data['channel']
-        thread_ts = data['ts']
-        user = data['user'] # This is not username but user ID (the format is either U*** or W***)
 
-        web_client.chat_postMessage(
-                channel=channel_id,
-                text=response
-        )
+##########################################################################################
+## Our thread-safe dictionary
+##########################################################################################
+
+
+class TempDict:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.data = {}
+
+    def __getitem__(self, key):
+        with self.lock:
+            return self.data[key]
+
+    def __setitem__(self, key, value):
+        with self.lock:
+            self.data[key] = value
+
+    def __len__(self):
+        with self.lock:
+            return len(self.data)
+
+
+# This is the actual dictionary we're going to populate
+temp_dict = TempDict()
+
+
+##########################################################################################
+## MQTT Section
+##########################################################################################
+
+
+def handle_temp_message(message):
+    # The message we get here looks like:
+    #   "MQTT publish: topic 'ps1/zigbee2mqtt/tempsense-lasers', payload '{\"battery\":100,\"humidity\":24.09,\"linkquality\":65,\"temperature\":17.39,\"voltage\":3100}'"
+    # which we split on ', payload' so the resulting array has
+    # the area in msg_parts[0], and the actual payload in msg_parts[1]
+    # that we will convert to json
+    msg_parts = message.split(", payload")
+
+    # The part that will give us the sensor name
+    # This crazy line is simply to get us directly to the sensor
+    # name, skipping all the intermediate parts
+    area = msg_parts[0].split("'")[1::2][0].split("/")[2].split("-")[1]
+    # And now the actual json payload
+    payload = json.loads(msg_parts[1].strip().replace("'", ""))
+
+    # And add it to our dictionary
+    temp_dict[area] = payload
+
+    print(f"AREA: {area} ==> {temp_dict[area]}")
+
+
+# This function is invoked by the MQTT library when we get
+# a message
+def on_message(client, userdata, message):
+    try:
+        payload = str(message.payload.decode())
+        # convert string to a dictionary
+        payload_dict = json.loads(payload)
+        # There are lots of different types of payloads, we
+        # want the one that explicitly has the "message" key
+        # because we need to know what sensor we're working with,
+        # and if that key exists, then we only want the messages
+        # where "tempsense" is in the text, as those are the messages
+        # we're working with
+        if "message" in payload_dict and "tempsense" in payload_dict["message"]:
+            handle_temp_message(payload_dict["message"])
+    except Exception as e:
+        print("Got a message error: {str(e)}")
+
+
+def setup_mqtt_reader():
+    # Initialize the MQTT client
+    client = mqtt.Client()
+    client.connect(config["MQTT"]["broker"], int(config["MQTT"]["port"]))
+
+    # Subscribe to the MQTT topic
+    client.subscribe(config["MQTT"]["topic"])
+
+    # Add the message callback function to the MQTT client
+    client.on_message = on_message
+
+    # Start the MQTT client loop to receive messages
+    client.loop_forever()
 
 
 ##########################################################################################
@@ -231,10 +234,10 @@ print("Starting the mqtt thread...")
 mqtt_reader_thread = threading.Thread(target=setup_mqtt_reader)
 mqtt_reader_thread.start()
 
-# Now let's set up the Slack part
-print("Starting the slack thread...")
-rtm_client = RTMClient(token=config['Slack']['token'])
-rtm_client.start()
-
 # And listen on the mqtt topic forever...
-mqtt_reader_thread.join()
+# mqtt_reader_thread.join()
+
+# Now let's set up the Discord part
+print("Starting the Discord thread...")
+# Run the bot
+bot.run(config["Discord"]["token"])
